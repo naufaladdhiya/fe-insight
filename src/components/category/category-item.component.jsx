e6628273d657020f3961573af87adf794adad3c2
egarahmatul <egarahmatul0775@gmail.com>
2023-06-18 21:52:55 +0700

    edit latop json dan edit card component

--- src/components/category/category-item.component.jsx
+++ src/components/category/category-item.component.jsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
 
 const CategoryItem = ({ category }) => {
-  const { name, id, description } = category;
+  const { photo, name, id, description } = category;
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
-        <img src="https://source.unsplash.com/random/900×700/?web" alt={name} />
+        <img src={photo} alt={name}  />
       </figure>
       <div key={id} className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 className="card-title justify-center items-center">{name}</h2>
         <p className="justify-center items-center">{description}</p>
-        <div className="card-actions justify-end items-center">
-          <Link to={`/category/${id}`}>
-            <button className="btn btn-primary ">{name}</button>
-          </Link>
+        <div className="card-actions justify-end justify-center items-center">
+          <button className="btn btn-primary ">
+            <Link to={`/category/${id}`}>{name}</Link>
+          </button>
         </div>
       </div>
     </div>
